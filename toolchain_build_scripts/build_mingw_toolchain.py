6b02d8b8387a9d2dbc98c1b1b3076dae21dd51a4
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-01-09 20:09:42 +0100

    Update patch URL in buildscript

--- toolchain_build_scripts/build_mingw_toolchain.py
+++ toolchain_build_scripts/build_mingw_toolchain.py
@@ -83,8 +83,8 @@ SOURCES['gcc'] = {
 	'version'   : '7.2.0',
 	'url' : 'https://ftp.gnu.org/gnu/gcc/gcc-{version}/gcc-{version}.tar.xz',
 	'patches' : [
-		( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/mingw_build_scripts/patches/0001-gcc_7_1_0_weak_refs_x86_64.patch', 'p1' ),
-		#( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/mingw_build_scripts/patches/0140-gcc-7-Enable-std-experimental-filesystem.patch', 'p1' ), #Unable to get this to work.
+		( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/toolchain_build_scripts/patches/0001-gcc_7_1_0_weak_refs_x86_64.patch', 'p1' ),
+		#( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/toolchain_build_scripts/patches/0140-gcc-7-Enable-std-experimental-filesystem.patch', 'p1' ), #Unable to get this to work.
 	],
 	'softlink_to_package' : [
 		('gmp'  , 'gmp' ),
