3f900719801865162dd37ad838181c9e6f0c51fa
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-02-20 03:14:03 +0100

    reformat mingw script header

--- toolchain_build_scripts/build_mingw_toolchain.py
+++ toolchain_build_scripts/build_mingw_toolchain.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # ####################################################
-# Copyright (C) 2017 DeadSix27 (https://github.com/DeadSix27/python_cross_compile_script)
-#
+# Copyright (C) 2018 DeadSix27 (https://github.com/DeadSix27/python_cross_compile_script)
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ###################################################
-
 
 #pip modules
 import progressbar # Please run: pip3 install progressbar2
