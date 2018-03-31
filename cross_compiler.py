fb3c1b5299bb6409868a3cb89add982236a4b29a
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-03-31 09:32:13 +0200

    fix x264 not detecting libav*
    I wonder how many other libraries have this pkg config issue...
    
    fixes #57

--- cross_compiler.py
+++ cross_compiler.py
@@ -2003,9 +2003,12 @@ PRODUCTS = {
 		'repo_type' : 'git',
 		'url' : 'https://git.videolan.org/git/x264.git',
 		'configure_options': '--host={target_host} --enable-static --cross-prefix={cross_prefix_bare} --prefix={product_prefix}/x264.installed --enable-strip --bit-depth=all',
-		'depends_on' : (
+		'env_exports' : {
+			'PKGCONFIG' : 'pkg-config',
+		},
+		'depends_on' : [
 			'libffmpeg',
-		),
+		],
 		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'x264' },
 	},
 	'cuetools' : {
