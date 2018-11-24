a696af18992df89c244a1f20102374bd04e7d4ce
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-11-24 20:03:29 +0100

    scxvid: fix building

--- cross_compiler.py
+++ cross_compiler.py
@@ -2161,12 +2161,11 @@ PRODUCTS = {
 	'scxvid' : {
 		'repo_type' : 'git',
 		'url' : 'https://github.com/DeadSix27/SCXvid-standalone',
-		'needs_configure' : False,
 		'conf_system' : 'cmake',
 		'source_subfolder' : 'build',
 		'configure_options': '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={product_prefix}/SCXvid-standalone_git.installed',
 		'run_post_install': [
-			'{cross_prefix_bare}strip -v {product_prefix}/scxvid.installed/bin/scxvid.exe',
+			'{cross_prefix_bare}strip -v {product_prefix}/SCXvid-standalone_git.installed/bin/scxvid.exe',
 		],
 		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'SCXvid-standalone' },
 	},
