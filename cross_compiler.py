268a5beb30a8647418c3dbd1f2fe8aca75a1228c
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-11-25 15:35:02 +0100

    libtheora: disable tests,doc and examplesy

--- cross_compiler.py
+++ cross_compiler.py
@@ -3695,7 +3695,7 @@ DEPENDS = {
 		'patches' : (
 			('https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/patches/theora_remove_rint_1.2.0alpha1.patch', '-p1'),
 		),
-		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
+		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-doc --disable-spec --disable-oggtest --disable-vorbistest --disable-examples',
 		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'theora' },
 	},
 	'freetype' : {
