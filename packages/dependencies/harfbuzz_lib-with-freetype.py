66b1686704dcd73b12e0027d440eb7c6447e64a6
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-02 22:05:48 +0100

    harfbuzz: update to 2.2.0

--- packages/dependencies/harfbuzz_lib-with-freetype.py
+++ packages/dependencies/harfbuzz_lib-with-freetype.py
@@ -2,8 +2,8 @@
 	'repo_type' : 'archive',
 	'download_locations' : [
 		#UPDATECHECKS https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D
-		{ "url" : "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.1.3.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "613264460bb6814c3894e3953225c5357402915853a652d40b4230ce5faf0bee" }, ], },
-		{ "url" : "https://fossies.org/linux/misc/harfbuzz-2.1.3.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "613264460bb6814c3894e3953225c5357402915853a652d40b4230ce5faf0bee" }, ], },
+		{ "url" : "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.2.0.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "49b8d984d2e9a0157ffaf880d4bdeda4207aff6431d85303df77a0d4" }, ], },
+		{ "url" : "https://fossies.org/linux/misc/harfbuzz-2.2.0.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "49b8d984d2e9a0157ffaf880d4bdeda4207aff6431d85303df77a0d4" }, ], },
 	],
 	'run_post_install': [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lharfbuzz.*/Libs: -L${{libdir}} -lharfbuzz -lfreetype/\' "{pkg_config_path}/harfbuzz.pc"',
@@ -11,5 +11,5 @@
 	'folder_name' : 'harfbuzz-with-freetype',
 	'rename_folder' : 'harfbuzz-with-freetype',
 	'configure_options': '--host={target_host} --prefix={target_prefix} --with-freetype --with-fontconfig=no --disable-shared --with-icu=no --with-glib=no --with-gobject=no --disable-gtk-doc-html',
-	'_info' : { 'version' : '2.1.3', 'fancy_name' : 'harfbuzz (with freetype2)' },
+	'_info' : { 'version' : '2.2.0', 'fancy_name' : 'harfbuzz (with freetype2)' },
 }
--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -2,12 +2,12 @@
 	'repo_type' : 'archive',
 	'download_locations' : [
 		#UPDATECHECKS https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D
-		{ "url" : "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.1.3.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "613264460bb6814c3894e3953225c5357402915853a652d40b4230ce5faf0bee" }, ], },
-		{ "url" : "https://fossies.org/linux/misc/harfbuzz-2.1.3.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "613264460bb6814c3894e3953225c5357402915853a652d40b4230ce5faf0bee" }, ], },
+		{ "url" : "https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.2.0.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "49b8d984d2e9a0157ffaf880d4bdeda4207aff6431d85303df77a0d4" }, ], },
+		{ "url" : "https://fossies.org/linux/misc/harfbuzz-2.2.0.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "49b8d984d2e9a0157ffaf880d4bdeda4207aff6431d85303df77a0d4" }, ], },
 	],
 	'run_post_install': [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lharfbuzz.*/Libs: -L${{libdir}} -lharfbuzz -lfreetype/\' "{pkg_config_path}/harfbuzz.pc"',
 	],
 	'configure_options': '--host={target_host} --prefix={target_prefix} --without-freetype --with-fontconfig=no --disable-shared --with-icu=no --with-glib=no --with-gobject=no --disable-gtk-doc-html', #--with-graphite2 --with-cairo --with-icu --with-gobject
-	'_info' : { 'version' : '2.1.3', 'fancy_name' : 'harfbuzz' },
+	'_info' : { 'version' : '2.2.0', 'fancy_name' : 'harfbuzz' },
 }
