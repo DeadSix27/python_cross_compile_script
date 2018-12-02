4aa34a0ad8df407ed24d851bafe8c670345fc24a
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-02 22:38:13 +0100

    gnutls: back to 3.6.4 until nettle is updated

--- packages/dependencies/gnutls.py
+++ packages/dependencies/gnutls.py
@@ -2,8 +2,8 @@
 	'repo_type' : 'archive',
 	'download_locations' : [
 		#UPDATECHECKS: https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/
-		{ "url" : "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.5.tar.xz", "hashes" : [ { "type" : "sha256", "sum" : "c0ee1442876d022ab93fa111cd072ce131519325f6ede55c4930463e" }, ], },
-		{ "url" : "https://fossies.org/linux/misc/gnutls-3.6.5.tar.xz", "hashes" : [ { "type" : "sha256", "sum" : "c0ee1442876d022ab93fa111cd072ce131519325f6ede55c4930463e" }, ], },
+		{ "url" : "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.4.tar.xz", "hashes" : [ { "type" : "sha256", "sum" : "c663a792fbc84349c27c36059181f2ca86c9442e75ee8b0ad72f5f9b35deab3a" }, ], },
+		{ "url" : "https://fossies.org/linux/misc/gnutls-3.6.4.tar.xz", "hashes" : [ { "type" : "sha256", "sum" : "c663a792fbc84349c27c36059181f2ca86c9442e75ee8b0ad72f5f9b35deab3a" }, ], },
 	],
 	'configure_options':
 		'--host={target_host} --prefix={target_prefix} --disable-shared --enable-static '
@@ -36,12 +36,11 @@
 	'run_post_install': [
 		'sed -i.bak \'s/-lgnutls *$/-lgnutls -lnettle -lhogweed -lgmp -lcrypt32 -lws2_32 -liconv/\' "{pkg_config_path}/gnutls.pc"', #TODO -lintl
 	],
-	'patches' : [
-		('https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/patches/gnutls/gnutls-3.6.5-use-nettle34.patch', '-p1'),
+	# 'patches' : [
 		# ('https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/patches/gnutls/0001-gnutls-3.5.11-arpainet_pkgconfig.patch', '-p1'),
 		# ('https://raw.githubusercontent.com/Martchus/PKGBUILDs/master/gnutls/mingw-w64/gnutls-3.2.7-rpath.patch','-p1'),
 		# ('https://raw.githubusercontent.com/Martchus/PKGBUILDs/master/gnutls/mingw-w64/gnutls-fix-external-libtasn1-detection.patch','-p1'),
-	],
+	# ],
 	'depends_on' : [
 		'gmp',
 		'libnettle',
