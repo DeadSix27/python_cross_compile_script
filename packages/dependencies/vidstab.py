4b9fb8bf602110467f156847d004706bcb1106d1
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-04 03:50:20 +0100

    vidstab: fix naming

--- packages/dependencies/vidstab.py
+++ packages/dependencies/vidstab.py
@@ -1,6 +1,7 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/georgmartius/vid.stab.git', #"Latest commit 97c6ae2  on May 29, 2015" .. master then I guess?
+	'rename_folder' : 'vidstab_git',
 	'conf_system' : 'cmake',
 	'configure_options' : '{cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_SHARED=OFF -DCMAKE_AR={cross_prefix_full}ar -DUSE_OMP=OFF', #fatal error: omp.h: No such file or directory
 	'run_post_patch' : [
