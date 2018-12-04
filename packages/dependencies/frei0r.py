63ddd650f4b88e76cf106158012feb5964cf8c20
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-04 04:09:13 +0100

    ffmpeg: add dlfcn
    ffmpeg now requires dlfcn.h to build with frei0r and ladspa

--- packages/dependencies/frei0r.py
+++ packages/dependencies/frei0r.py
@@ -5,6 +5,7 @@
 		{ 'url' : 'https://files.dyne.org/frei0r/frei0r-plugins-1.6.1.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e0c24630961195d9bd65aa8d43732469e8248e8918faa942cfb881769d11515e' }, ], },
 		{ 'url' : 'https://ftp.osuosl.org/pub/blfs/conglomeration/frei0r/frei0r-plugins-1.6.1.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e0c24630961195d9bd65aa8d43732469e8248e8918faa942cfb881769d11515e' }, ], },
 	],
+	'depends_on' : [ 'dlfcn-win32', ],
 	'conf_system' : 'cmake',
 	'run_post_patch' : [ # runs commands post the patch process
 		'sed -i.bak "s/find_package (Cairo)//g" CMakeLists.txt', #idk
