320c6a795be706b5340137b3e3e2b6d8d96fcc49
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-04 07:35:16 +0100

    srt: disable shared

--- packages/dependencies/srt.py
+++ packages/dependencies/srt.py
@@ -4,7 +4,7 @@
 	#'branch' : '16d77ad6b4249c3ba3b812d26c4cbb356300f908',
 	'source_subfolder' : '_build',
 	'conf_system' : 'cmake',
-	'depends_on' : [ 'libressl' ],
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS:bool=off -DBUILD_TESTS=no',
+	'depends_on' : [ 'gnutls' ],
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_GNUTLS=1 -DENABLE_SHARED=0',
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libmysofa' },
 }
