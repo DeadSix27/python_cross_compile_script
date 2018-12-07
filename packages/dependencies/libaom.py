624df582ff64d81976ad25bc2c099e075040cdee
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-07 12:40:07 +0100

    packages/aom: update and cleanup build options

--- packages/dependencies/libaom.py
+++ packages/dependencies/libaom.py
@@ -1,20 +1,25 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://aomedia.googlesource.com/aom',
-	'branch' : '67aec0fc602b068216b43e0fe5060e5eb903bcb8',
+	'branch' : '2d7eb8bce5c413e0944917cdfd378c26f4e3f85b',
 	'conf_system' : 'cmake',
 	'source_subfolder' : 'build',
-	'configure_options' : '.. {cmake_prefix_options} '
-		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
-		'-DCONFIG_LOWBITDEPTH=0 -DCONFIG_HIGHBITDEPTH=1 '
-		'-DCONFIG_AV1=1 -DHAVE_PTHREAD=1 -DBUILD_SHARED_LIBS=0 -DENABLE_DOCS=0 -DCONFIG_INSTALL_DOCS=0 '
-		'-DCONFIG_INSTALL_BINS=0 -DCONFIG_INSTALL_LIBS=1 '
-		'-DCONFIG_INSTALL_SRCS=1 -DCONFIG_UNIT_TESTS=0 -DENABLE_TESTS=0 -DENABLE_TESTDATA=0 '
-		'-DCONFIG_AV1_DECODER=1 -DCONFIG_AV1_ENCODER=1 '
-		'-DCONFIG_MULTITHREAD=1 -DCONFIG_PIC=1 -DCONFIG_COEFFICIENT_RANGE_CHECKING=0 '
-		'-DCONFIG_RUNTIME_CPU_DETECT=1 -DCONFIG_WEBM_IO=1 '
-		'-DCONFIG_SPATIAL_RESAMPLING=1 -DENABLE_NASM=off '
-		'-DLIBXML_STATIC=1 -DGLIB_STATIC_COMPILATION=1'
+	'configure_options' : 
+		'.. {cmake_prefix_options} '
+		'-DCMAKE_INSTALL_PREFIX={product_prefix}/aom.installed '
+		'-DBUILD_SHARED_LIBS=0 '
+		'-DENABLE_DOCS=0 '
+		'-DENABLE_TESTS=0 '
+		'-DENABLE_TOOLS=0 '
+		'-DENABLE_EXAMPLES=0 '
+		'-DENABLE_TESTDATA=0 '
+		'-DCONFIG_AV1_DECODER=1 '
+		'-DCONFIG_AV1_ENCODER=1 '
+		'-DCONFIG_PIC=1 '
+		'-DCONFIG_SPATIAL_RESAMPLING=1 '
+		'-DENABLE_NASM=off ' # YASM is preferred (default)
+		'-DCONFIG_STATIC=1 '
+		'-DCONFIG_SHARED=0'
 	,
 	'depends_on' : [ 'libxml2' ],
 	'update_check' : { 'type' : 'git', },
