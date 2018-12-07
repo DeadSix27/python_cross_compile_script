89f38906ecba75622ca296b818c8865027d4a500
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-07 18:13:29 +0100

    packages/lensfun: add missing dependency

--- packages/dependencies/lensfun.py
+++ packages/dependencies/lensfun.py
@@ -10,6 +10,6 @@
 		'sed -i.bak \'s/Libs: -L${{libdir}} -llensfun.*/Libs: -L${{libdir}} -llensfun -lstdc++/\' "../libs/lensfun/lensfun.pc.cmake"',
 	],
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_INSTALL_DATAROOTDIR={target_prefix}/share -DBUILD_DOC=0 -DBUILD_STATIC=1 -DBUILD_SHARED_LIBS=0 -DBUILD_TESTS=0 -DBUILD_LENSTOOL=0',
-	'depends_on': [ 'libpng' ],
+	'depends_on': [ 'libpng','glib2' ],
 	'_info' : { 'version' : None, 'fancy_name' : 'lensfun (library)' },
 }
