c44ddddb51c2a2248417c0fcd1493f3ba57539e6
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-08 15:02:22 +0100

    packages/x265: cleanup

--- packages/dependencies/libx265.py
+++ packages/dependencies/libx265.py
@@ -2,9 +2,16 @@
 	'repo_type' : 'mercurial',
 	'url' : 'https://bitbucket.org/multicoreware/x265',
 	'rename_folder' : 'libx265_hg',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_ASSEMBLY=ON -DENABLE_CLI:BOOL=OFF -DENABLE_SHARED=OFF -DCMAKE_AR={cross_prefix_full}ar -DLIBXML_STATIC=ON -DGLIB_STATIC_COMPILATION=ON', # no cli, as this is just for the library.
+	'source_subfolder' : '_build',
+	'configure_options' : 
+		'../source {cmake_prefix_options} '
+		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
+		'-DENABLE_ASSEMBLY=ON '
+		'-DENABLE_CLI:BOOL=OFF '
+		'-DENABLE_SHARED=OFF '
+		'-DCMAKE_AR={cross_prefix_full}ar'
+	,
 	'conf_system' : 'cmake',
-	'source_subfolder' : 'source',
 	'depends_on' : [ 'libxml2' ],
 	'run_post_install' : [
 		'sed -i.bak \'s|-lmingwex||g\' "{pkg_config_path}/x265.pc"',
--- packages/dependencies/libx265_multibit_12.py
+++ packages/dependencies/libx265_multibit_12.py
@@ -2,8 +2,18 @@
 	'repo_type' : 'mercurial',
 	'url' : 'https://bitbucket.org/multicoreware/x265',
 	'rename_folder' : 'libx265_hg_12bit',
-	'source_subfolder' : 'source',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_AR={cross_prefix_full}ar -DENABLE_ASSEMBLY=ON -DHIGH_BIT_DEPTH=ON -DEXPORT_C_API=OFF -DENABLE_SHARED=OFF -DENABLE_CLI=OFF -DMAIN12=ON -DCMAKE_INSTALL_PREFIX={offtree_prefix}/libx265_12bit -DLIBXML_STATIC=ON -DGLIB_STATIC_COMPILATION=ON',
+	'source_subfolder' : '_build',
+	'configure_options' : 
+		'../source {cmake_prefix_options} '
+		'-DCMAKE_AR={cross_prefix_full}ar '
+		'-DENABLE_ASSEMBLY=ON '
+		'-DHIGH_BIT_DEPTH=ON '
+		'-DEXPORT_C_API=OFF '
+		'-DENABLE_SHARED=OFF '
+		'-DENABLE_CLI=OFF '
+		'-DMAIN12=ON '
+		'-DCMAKE_INSTALL_PREFIX={offtree_prefix}/libx265_12bit'
+	,
 	'run_post_install' : [
 		'mv -fv "{offtree_prefix}/libx265_12bit/lib/libx265.a" "{offtree_prefix}/libx265_12bit/lib/libx265_main12.a"'
 	],
