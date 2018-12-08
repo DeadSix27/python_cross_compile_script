4ff1cd03e52f1b872f0ca9655b8ee46fb6fde45a
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-08 06:32:12 +0100

    packages/openjp2: fix building with libpng and tiff

--- packages/dependencies/libopenjpeg.py
+++ packages/dependencies/libopenjpeg.py
@@ -2,6 +2,13 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/uclouvain/openjpeg.git',
 	'conf_system' : 'cmake',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS:bool=off',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS:bool=off',
+	'depends_on' : [
+		'zlib','libpng', 'libtiff', 'lcms2'
+	],
+	'patches' : [
+		( 'openjpeg2/use-PkgConfig-for-third-party-libraries.patch', '-p1', '..' ),
+	],
 	'_info' : { 'version' : None, 'fancy_name' : 'openjpeg' },
 }
