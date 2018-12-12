b0e3e93c3af70e42edf110e09039815575195c00
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-12 11:26:34 +0100

    Revert "packages/spirvtools: stick to last working commit"
    This reverts commit cbaac43c95226b4ca5c9c1478467069966e9ef35.

--- packages/dependencies/spirv_tools.py
+++ packages/dependencies/spirv_tools.py
@@ -2,7 +2,6 @@
 	'repo_type' : 'git',
 	'rename_folder' : 'spirv-tools',
 	'url' : 'https://github.com/KhronosGroup/SPIRV-Tools.git',
-	'branch' : 'aa270e568a3fd637f4a329611107b384a8023195',
 	'needs_make' : False,
 	'needs_make_install' : False,
 	'needs_configure' : False,
