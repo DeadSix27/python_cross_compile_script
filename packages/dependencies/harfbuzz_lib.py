7ecad3045b7baf4f79e71dcb91c50e8015003631
DeadSix27 <DeadSix27@users.noreply.github.com>
2018-12-21 12:42:57 +0100

    packages/harfbuzz: update to 2.3.0

--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -1,13 +1,13 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.2.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b7ccfcbd56b970a709e8b9ea9fb46c922c606c2feef8f086fb6a8492e530f810' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.2.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b7ccfcbd56b970a709e8b9ea9fb46c922c606c2feef8f086fb6a8492e530f810' }, ], },
+		{ 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.3.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3b314db655a41d19481e18312465fa25fca6f63382217f08062f126059f96764' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.3.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3b314db655a41d19481e18312465fa25fca6f63382217f08062f126059f96764' }, ], },
 	],
 	'run_post_install' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lharfbuzz.*/Libs: -L${{libdir}} -lharfbuzz -lfreetype/\' "{pkg_config_path}/harfbuzz.pc"',
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --without-freetype --with-fontconfig=no --disable-shared --with-icu=no --with-glib=no --with-gobject=no --disable-gtk-doc-html', #--with-graphite2 --with-cairo --with-icu --with-gobject
 	'update_check' : { 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'harfbuzz-(?P<version_num>[\d.]+)\.tar\.bz2' },
-	'_info' : { 'version' : '2.2.0', 'fancy_name' : 'harfbuzz' },
+	'_info' : { 'version' : '2.3.0', 'fancy_name' : 'harfbuzz' },
 }
