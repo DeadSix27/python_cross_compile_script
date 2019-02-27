5f65b8addd4c3f008b653202364c618c81d97295
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-02-27 11:10:55 +0100

    packages/fontconfig: build from master again

--- packages/dependencies/fontconfig.py
+++ packages/dependencies/fontconfig.py
@@ -1,7 +1,7 @@
 {
 	'repo_type' : 'git',
 	'do_not_bootstrap' : True,
-	'branch' : 'b047e299546ac3abb79cf0bac3c67f5c2dfc7fb6',
+	# 'branch' : '40e27f5d989ef4e40d218624de0a51de3de43177',
 	'url' : 'https://gitlab.freedesktop.org/fontconfig/fontconfig.git',
 	'folder_name' : 'fontconfig_git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --enable-libxml2 --disable-shared --enable-static --disable-docs --disable-silent-rules',
