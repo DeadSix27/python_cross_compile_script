a4965b8244f63eaa7284b610886ceebfe46ec9e0
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-11 10:02:52 +0200

    packages/vulkan_headers: build from master again

--- packages/dependencies/vulkan_headers.py
+++ packages/dependencies/vulkan_headers.py
@@ -1,5 +1,6 @@
 {
 	'repo_type' : 'git',
+	#'branch' : '16a43fcfe42dc8c7565754b1df5d575b540a876a',
 	'url' : 'https://github.com/KhronosGroup/Vulkan-Headers.git',
 	'recursive_git' : True,
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={target_prefix}',
