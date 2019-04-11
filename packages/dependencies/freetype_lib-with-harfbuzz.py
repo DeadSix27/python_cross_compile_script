35a78cbac9238ded3cf7784b6a257e83d88fecfa
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-11 09:58:25 +0200

    packages/freetype: update to 2.10.0

--- packages/dependencies/freetype_lib-with-harfbuzz.py
+++ packages/dependencies/freetype_lib-with-harfbuzz.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.9.1/freetype-2.9.1.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.9.1.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d' }, ], },
+		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.10.0/freetype-2.10.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.10.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06' }, ], },
 	],
 	'folder_name' : 'freetype-with-harfbuzz',
 	'rename_folder' : 'freetype-with-harfbuzz',
@@ -11,5 +11,5 @@
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lfreetype.*/Libs: -L${{libdir}} -lfreetype -lbz2 -lharfbuzz/\' "{pkg_config_path}/freetype2.pc"',
 	],
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/', 'type' : 'sourceforge', },
-	'_info' : { 'version' : '2.9.1', 'fancy_name' : 'freetype2' },
+	'_info' : { 'version' : '2.10.0', 'fancy_name' : 'freetype2' },
 }
--- packages/dependencies/freetype_lib.py
+++ packages/dependencies/freetype_lib.py
@@ -1,10 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.9.1.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d' }, ], },
-		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.9.1/freetype-2.9.1.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'db8d87ea720ea9d5edc5388fc7a0497bb11ba9fe972245e0f7f4c7e8b1e1e84d' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.10.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06' }, ], },
+		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.10.0/freetype-2.10.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06' }, ], },
 	],
 	'configure_options' : '--host={target_host} --build=x86_64-linux-gnu --prefix={target_prefix} --disable-shared --enable-static --with-zlib={target_prefix} --without-png --with-harfbuzz=no',
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/', 'type' : 'sourceforge', },
-	'_info' : { 'version' : '2.9.1', 'fancy_name' : 'freetype2' },
+	'_info' : { 'version' : '2.10.0', 'fancy_name' : 'freetype2' },
 }
