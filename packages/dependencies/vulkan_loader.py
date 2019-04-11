c446528e891196230f21f76a4a1e1f53532c1bb5
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-11 10:03:09 +0200

    packages/vulkan_loader: use subdir for building

--- packages/dependencies/vulkan_loader.py
+++ packages/dependencies/vulkan_loader.py
@@ -1,17 +1,20 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/KhronosGroup/Vulkan-Loader.git',
-	'recursive_git' : True, 
+	# 'recursive_git' : True, 
 	'configure_options' : 
-		'. {cmake_prefix_options} -DVULKAN_HEADERS_INSTALL_DIR={target_prefix} '
+		'.. {cmake_prefix_options} -DVULKAN_HEADERS_INSTALL_DIR={target_prefix} '
+		'-DCMAKE_BUILD_TYPE=Release '
+		'-DBUILD_TESTS=OFF '
 		'-DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_ASM-ATT_COMPILER={mingw_binpath}/{cross_prefix_bare}as '
 		'-DBUILD_TESTS=OFF -DENABLE_STATIC_LOADER=ON '
 		'-DCMAKE_C_FLAGS=\'-D_WIN32_WINNT=0x0600 -D__STDC_FORMAT_MACROS\' '
 		'-DCMAKE_CXX_FLAGS=\'-D__USE_MINGW_ANSI_STDIO -D__STDC_FORMAT_MACROS -fpermissive -D_WIN32_WINNT=0x0600\''
 	, #-D_WIN32_WINNT=0x0600 -D__STDC_FORMAT_MACROS" -D__USE_MINGW_ANSI_STDIO -D__STDC_FORMAT_MACROS -fpermissive -D_WIN32_WINNT=0x0600"
 	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
 	'patches' : [
-		('vulkan/0001-vulkan-loader-cross-compile-static-linking-hacks.patch','-p1'),
+		('vulkan/0001-vulkan-loader-cross-compile-static-linking-hacks.patch','-p1','..'),
 	],
 	'run_post_install' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lvulkan/Libs: -L${{libdir}} -lvulkan -lshlwapi -lcfgmgr32/\' "{target_prefix}/lib/pkgconfig/vulkan.pc"',
