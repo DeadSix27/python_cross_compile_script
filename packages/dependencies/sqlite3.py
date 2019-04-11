c0225ba253e911b0e50b2053c2bb7b1d799c7e70
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-11 10:06:42 +0200

    packages/sqlite3: update to 3.27.2

--- packages/dependencies/sqlite3.py
+++ packages/dependencies/sqlite3.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ "url" : "https://www.sqlite.org/2018/sqlite-autoconf-3260000.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d" }, ], },
-		{ "url" : "https://fossies.org/linux/misc/sqlite-autoconf-3260000.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "5daa6a3fb7d1e8c767cd59c4ded8da6e4b00c61d3b466d0685e35c4dd6d7bf5d" }, ], },
+		{ 'url' : 'https://www.sqlite.org/2019/sqlite-autoconf-3270200.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50c39e85ea28b5ecfdb3f9e860afe9ba606381e21836b2849efca6a0bfe6ef6e' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3270200.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50c39e85ea28b5ecfdb3f9e860afe9ba606381e21836b2849efca6a0bfe6ef6e' }, ], },
 	],
 	'cflag_addition' : '-fexceptions -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_USE_MALLOC_H=1 -DSQLITE_USE_MSIZE=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing',
 	'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-threadsafe --disable-editline --enable-readline --enable-json1 --enable-fts5 --enable-session',
@@ -10,5 +10,5 @@
 		'zlib',
 	),
 	'update_check' : { 'url' : 'https://www.sqlite.org/index.html', 'type' : 'httpregex', 'regex' : r'<a href="releaselog/.*\.html">Version (?P<version_num>[\d.]+)<\/a>' },
-	'_info' : { 'version' : '3.26.0', 'fancy_name' : 'libsqlite3' },
+	'_info' : { 'version' : '3.27.2', 'fancy_name' : 'libsqlite3' },
 }
