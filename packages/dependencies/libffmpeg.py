6a6596d1b2f29d290b25593450461fa315a7a53a
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-12 13:20:42 +0200

    packages/libffmpeg: fix pc files
    Seems like --extra-libs doesn't add the .. extra lib to the pkg-config file.

--- packages/dependencies/libffmpeg.py
+++ packages/dependencies/libffmpeg.py
@@ -4,5 +4,12 @@
 	'rename_folder' : 'libffmpeg_git',
 	'configure_options' : '!VAR(ffmpeg_base_config)VAR! --prefix={target_prefix} --disable-shared --enable-static --disable-doc --disable-programs --enable-amf',
 	'depends_on' : [ 'ffmpeg_depends' ],
+	'run_post_install' : [
+		'sed -i.bak \'s/-luser32 -ldl/-luser32 -lpsapi -ldl/\' "{pkg_config_path}/libavcodec.pc"',
+		'sed -i.bak \'s/-lbz2 -ldl/-lbz2 -lpsapi -ldl/\' "{pkg_config_path}/libavfilter.pc"',
+		'sed -i.bak \'s/-lws2_32 -ldl/-lws2_32 -lpsapi -ldl/\' "{pkg_config_path}/libavformat.pc"',
+		'sed -i.bak \'s/-lbcrypt -ldl/-lbcrypt -lpsapi -ldl/\' "{pkg_config_path}/libavutil.pc"',
+		'sed -i.bak \'s/-luuid -static-libgcc -ldl/-luuid -static-libgcc -lpsapi -ldl/\' "{pkg_config_path}/libavdevice.pc"',
+	],
 	'_info' : { 'version' : None, 'fancy_name' : 'FFmpeg (library)' },
 }
