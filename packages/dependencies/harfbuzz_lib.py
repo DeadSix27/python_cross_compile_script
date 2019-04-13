bf59556eb77865e220dbfcd55ccdbc63358af792
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-13 10:35:38 +0200

    packages/harfbuzz: the freetypeless version needs to libfreetype..

--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -2,9 +2,6 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/harfbuzz/harfbuzz',
 	'branch' : '2.4.0',
-	'run_post_install' : [
-		'sed -i.bak \'s/Libs: -L${{libdir}} -lharfbuzz.*/Libs: -L${{libdir}} -lharfbuzz -lfreetype/\' "{pkg_config_path}/harfbuzz.pc"',
-	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --without-freetype --with-fontconfig=no --disable-shared --with-icu=no --with-glib=no --with-gobject=no --disable-gtk-doc-html', #--with-graphite2 --with-cairo --with-icu --with-gobject
 	'update_check' : { 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'harfbuzz-(?P<version_num>[\d.]+)\.tar\.bz2' },
 	'_info' : { 'version' : '2.4.0', 'fancy_name' : 'harfbuzz' },
