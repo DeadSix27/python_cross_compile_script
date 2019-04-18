ad6b29699746e749c9d3db254da4feceb07fcd4f
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-19 01:04:19 +0200

    packages/pcre2: update to 10.33

--- packages/dependencies/pcre2.py
+++ packages/dependencies/pcre2.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://ftp.pcre.org/pub/pcre/pcre2-10.32.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/pcre2-10.32.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f29e89cc5de813f45786580101aaee3984a65818631d4ddbda7b32f699b87c2e' }, ], },
+		{ 'url' : 'https://ftp.pcre.org/pub/pcre/pcre2-10.33.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '35514dff0ccdf02b55bd2e9fa586a1b9d01f62332c3356e379eabb75f789d8aa' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/pcre2-10.33.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '35514dff0ccdf02b55bd2e9fa586a1b9d01f62332c3356e379eabb75f789d8aa' }, ], },
 	],
 	'conf_system' : 'cmake',
 	'patches' : [
@@ -17,5 +17,5 @@
 		'bzip2', 'zlib',
 	],
 	'update_check' : { 'url' : 'ftp://ftp.pcre.org/pub/pcre/', 'type' : 'ftpindex', 'regex' : r'pcre2-(?P<version_num>[\d.]+)\.tar\.bz2' },
-	'_info' : { 'version' : '10.32', 'fancy_name' : 'pcre2' },
+	'_info' : { 'version' : '10.33', 'fancy_name' : 'pcre2' },
 }
