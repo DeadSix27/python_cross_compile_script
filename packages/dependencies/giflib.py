e5c7c6b27c265f5bed41dca467f4046d59f1661c
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-04-24 11:25:58 +0200

    packages/gif: disable utilities

--- packages/dependencies/giflib.py
+++ packages/dependencies/giflib.py
@@ -8,8 +8,11 @@
 		{ 'url' : 'https://sourceforge.net/projects/giflib/files/giflib-5.1.9.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '292b10b86a87cb05f9dcbe1b6c7b99f3187a106132dd14f1ba79c90f561c3295' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/giflib-5.1.9.tar.bz2'				  , 'hashes' : [ { 'type' : 'sha256', 'sum' : '292b10b86a87cb05f9dcbe1b6c7b99f3187a106132dd14f1ba79c90f561c3295' }, ], },
 	],
-	'patches' : [
-		( 'giflib/giflib-add-cmakelists.patch', '-p1', '..' ), # thanks to https://sourceforge.net/p/giflib/feature-requests/6/
+	# 'patches' : [
+		# ( 'giflib/giflib-add-cmakelists.patch', '-p1', '..' ), # thanks to https://sourceforge.net/p/giflib/feature-requests/6/
+	# ],
+	'copy_over' : [
+		'{project_root}/patches/giflib/CMakeLists.txt'
 	],
 	'source_subfolder' : '_build',
 	'configure_options' : 
