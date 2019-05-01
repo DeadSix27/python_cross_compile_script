7a53c3292bc349e6675fdb663c7887a24b6779cd
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-05-01 02:05:43 +0200

    packages/iconv: update to 1.16

--- packages/dependencies/iconv.py
+++ packages/dependencies/iconv.py
@@ -1,11 +1,11 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/libiconv-1.15.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178' }, ], },
+		{ 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/libiconv-1.16.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04' }, ], },
 	],
 	# CFLAGS=-O2
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-nls --enable-extra-encodings',
 	'update_check' : { 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'libiconv-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '1.15', 'fancy_name' : 'libiconv' },
+	'_info' : { 'version' : '1.16', 'fancy_name' : 'libiconv' },
 }
