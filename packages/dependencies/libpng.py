45f5597e45dcc0ced2773f92ce620a1b50c6ae92
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-05-04 17:53:13 +0200

    packages/libpng: update to 1.6.37

--- packages/dependencies/libpng.py
+++ packages/dependencies/libpng.py
@@ -1,17 +1,17 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://sourceforge.net/projects/libpng/files/libpng16/1.6.36/libpng-1.6.36.tar.xz',	'hashes' : [ { 'type' : 'sha256', 'sum' : 'eceb924c1fa6b79172fdfd008d335f0e59172a86a66481e09d4089df872aa319' },	], },
-		{ 'url' : 'https://fossies.org/linux/misc/libpng-1.6.36.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'eceb924c1fa6b79172fdfd008d335f0e59172a86a66481e09d4089df872aa319' }, ],	},
+		{ 'url' : 'https://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.xz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca' },	], },
+		{ 'url' : 'https://fossies.org/linux/misc/libpng-1.6.37.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca' }, ],	},
 	],
 	# 'custom_cflag' : '-fno-asynchronous-unwind-tables',
 	'conf_system' : 'cmake',
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DCMAKE_BUILD_TYPE=Release -DPNG_TESTS=OFF -DPNG_SHARED=OFF -DPNG_STATIC=ON',
 	# 'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --oldincludedir={target_prefix}/include',
 	'patches' : [
-		('libpng/libpng-1.6.36-apng.patch', '-p1'),
+		('libpng/libpng-1.6.37-apng.patch', '-p1'),
 	],
 	'depends_on' : [ 'zlib', ],
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/libpng/files/libpng16/', 'type' : 'sourceforge', },
-	'_info' : { 'version' : '1.6.36', 'fancy_name' : 'libpng' },
+	'_info' : { 'version' : '1.6.37', 'fancy_name' : 'libpng' },
 }
