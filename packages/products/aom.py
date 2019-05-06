510b51e17e2155fddb3e91b5b22b3dbe9826d8b9
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-05-06 13:51:22 +0200

    packages/aom: actually build the binaries..

--- packages/products/aom.py
+++ packages/products/aom.py
@@ -5,12 +5,14 @@
 	'source_subfolder' : 'build',
 	'configure_options' :
 		'.. {cmake_prefix_options} '
-		'-DCMAKE_INSTALL_PREFIX={product_prefix}/aom.installed '
+		'-DCMAKE_INSTALL_PREFIX={product_prefix}/aom_git.installed '
 		'-DBUILD_SHARED_LIBS=0 '
 		'-DENABLE_DOCS=0 '
 		'-DENABLE_TESTS=0 '
-		'-DENABLE_TOOLS=0 '
-		'-DENABLE_EXAMPLES=0 '
+		'-DENABLE_TOOLS=1 '
+		'-DENABLE_CCACHE=1 '
+		'-DCONFIG_LPF_MASK=1 '
+		'-DENABLE_EXAMPLES=1 '
 		'-DENABLE_TESTDATA=0 '
 		'-DCONFIG_AV1_DECODER=1 '
 		'-DCONFIG_AV1_ENCODER=1 '
