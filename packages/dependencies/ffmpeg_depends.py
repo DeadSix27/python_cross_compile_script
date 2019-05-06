d153220131bcf69a4186fd865d4e8b0e9efbe11e
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-05-06 13:50:30 +0200

    packages/ffmpeg: add missing vs dependency

--- packages/dependencies/ffmpeg_depends.py
+++ packages/dependencies/ffmpeg_depends.py
@@ -5,6 +5,7 @@
 		'libgme_game_music_emu', 'libwebp', 'tesseract', 'flite', 'libgsm', 'sdl2', 'libopus', 'opencore-amr', 'vo-amrwbenc', 'libogg', 'libspeex', 'davs2', 'openmpt',
 		'libvorbis', 'libtheora', 'freetype', 'expat', 'libxml2', 'libbluray', 'libxvid', 'xavs', 'xavs2', 'libsoxr', 'libx265_multibit', 'kvazaar', 'libaom',
 		'libdav1d', 'vamp_plugin', 'fftw3', 'libsamplerate', 'librubberband', 'liblame' ,'twolame', 'vidstab', 'libmysofa', 'libcaca', 'libmodplug',
-		'zvbi', 'libvpx', 'libilbc', 'libfribidi', 'gettext', 'libass', 'intel_quicksync_mfx', 'rtmpdump', 'libx264', 'libcdio', 'libcdio-paranoia', 'amf_headers', 'nv-codec-headers'
+		'zvbi', 'libvpx', 'libilbc', 'libfribidi', 'gettext', 'libass', 'intel_quicksync_mfx', 'rtmpdump', 'libx264', 'libcdio', 'libcdio-paranoia', 'amf_headers', 'nv-codec-headers',
+		'vapoursynth_libs',
 	],
 }
