0218b4b80830563c7aab2b1e6d561d20977f5fd4
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-05-09 14:51:32 +0200

    packages/opencl: fix building with newest commits

--- packages/dependencies/opencl_icd.py
+++ packages/dependencies/opencl_icd.py
@@ -3,11 +3,8 @@
 	'url' : 'https://github.com/KhronosGroup/OpenCL-ICD-Loader.git',
 	'needs_make_install' :False,
 	'conf_system' : 'cmake',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF',
-	'depends_on' : [ 'opencl_headers' ],
-	'run_post_patch' : [
-		'sed -i.bak \'s/Devpkey.h/devpkey.h/\' icd_windows_hkr.c',
-	],
+	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=ON',
+	'depends_on' : [ 'opencl_headers' ],	
 	'run_post_build' : [
 		'if [ ! -f "already_ran_make_install" ] ; then cp -vf "libOpenCL.dll.a" "{target_prefix}/lib/libOpenCL.dll.a" ; fi',
 		'if [ ! -f "already_ran_make_install" ] ; then touch already_ran_make_install ; fi',
