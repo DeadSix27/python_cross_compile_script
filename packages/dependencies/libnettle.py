2cf0936aaeacb0c0bb2be7ecac11a3a055368934
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-06-28 19:55:33 +0200

    packages/nettle: update to 3.5.1

--- packages/dependencies/libnettle.py
+++ packages/dependencies/libnettle.py
@@ -1,13 +1,13 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.4.1.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f941cf1535cd5d1819be5ccae5babef01f6db611f9b5a777bae9c7604b8a92ad' }, ], },
-		{ 'url' : 'https://fossies.org/linux/privat/nettle-3.4.1.tar.gz/', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f941cf1535cd5d1819be5ccae5babef01f6db611f9b5a777bae9c7604b8a92ad' }, ], },
+		{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.5.1.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '75cca1998761b02e16f2db56da52992aef622bf55a3b45ec538bc2eedadc9419' }, ], },
+		{ 'url' : 'https://fossies.org/linux/privat/nettle-3.5.1.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '75cca1998761b02e16f2db56da52992aef622bf55a3b45ec538bc2eedadc9419' }, ], },
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-openssl --with-included-libtasn1',
 	'depends_on' : [
 		'gmp',
 	],
 	'update_check' : { 'url' : 'https://ftp.gnu.org/gnu/nettle/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'nettle-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '3.4.1', 'fancy_name' : 'nettle' },
+	'_info' : { 'version' : '3.5.1', 'fancy_name' : 'nettle' },
 }
