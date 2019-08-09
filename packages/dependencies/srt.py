304ec6db34a705ba21f791e15ca7eefbdefaf814
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-08-09 18:41:17 +0200

    packages/srt: use gnutls again

--- packages/dependencies/srt.py
+++ packages/dependencies/srt.py
@@ -4,7 +4,7 @@
 	#'branch' : '16d77ad6b4249c3ba3b812d26c4cbb356300f908',
 	'source_subfolder' : '_build',
 	'conf_system' : 'cmake',
-	'depends_on' : [ 'gettext', 'libressl' ],
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=openssl -DENABLE_SHARED=0',
+	'depends_on' : [ 'gettext', 'gnutls' ],
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DENABLE_SHARED=0',
 	'_info' : { 'version' : None, 'fancy_name' : 'libmysofa' },
 }
