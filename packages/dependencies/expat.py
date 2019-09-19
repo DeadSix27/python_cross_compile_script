01aece0684709854a57658dc54120259b9c3a82e
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-09-19 17:13:33 +0200

    packages/expat: update to 2.2.8

--- packages/dependencies/expat.py
+++ packages/dependencies/expat.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://github.com/libexpat/libexpat/releases/download/R_2_2_7/expat-2.2.7.tar.bz2',	'hashes' : [ { 'type' : 'sha256', 'sum' : 'cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18' },	], },
-		{ 'url' : 'https://fossies.org/linux/www/expat-2.2.7.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'cbc9102f4a31a8dafd42d642e9a3aa31e79a0aedaa1f6efd2795ebc83174ec18' }, ],	},
+		{ 'url' : 'https://github.com/libexpat/libexpat/releases/download/R_2_2_8/expat-2.2.8.tar.xz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '61caa81a49d858afb2031c7b1a25c97174e7f2009aa1ec4e1ffad2316b91779b' },	], },
+		{ 'url' : 'https://fossies.org/linux/www/expat-2.2.8.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '61caa81a49d858afb2031c7b1a25c97174e7f2009aa1ec4e1ffad2316b91779b' }, ],	},
 	],
 	'env_exports' : {
 		'CPPFLAGS' : '-DXML_LARGE_SIZE',
@@ -14,5 +14,5 @@
 	],
 	'update_check' : { 'url' : 'https://github.com/libexpat/libexpat/releases', 'type' : 'githubreleases', 'name_or_tag' : 'name' },
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --without-docbook',
-	'_info' : { 'version' : '2.2.7', 'fancy_name' : 'expat' },
+	'_info' : { 'version' : '2.2.8', 'fancy_name' : 'expat' },
 }
