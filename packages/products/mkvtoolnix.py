f55ecb48939a70986c9b7502e2997b80662c67df
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-03 10:57:10 +0200

    packages/mkvtoolnix: re-enable and fix libfile dependency

--- packages/products/mkvtoolnix.py
+++ packages/products/mkvtoolnix.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'recursive_git' : True,
 	'build_system' : 'rake',
-	'url' : 'https://gitlab.com/mbunkus/mkvtoolnix.git', #why gitlab? At least its not sourceforge...
+	'url' : 'https://gitlab.com/mbunkus/mkvtoolnix.git',
 	'configure_options':
 		'--host={target_host} --prefix={product_prefix}/mkvtoolnix_git.installed --disable-shared --enable-static'
 		' --with-boost={target_prefix} --with-boost-system=boost_system --with-boost-filesystem=boost_filesystem --with-boost-date-time=boost_date_time --with-boost-regex=boost_regex --enable-optimization --enable-qt --enable-static-qt'
@@ -11,18 +11,18 @@
 	,
 	'build_options': '-v',
 	'depends_on' : [
-		'cmark','libfile','libflac','boost','qt5','gettext'
+		'cmark', 'libfile', 'libflac', 'boost', 'gettext'
 	],
 	'packages': {
 		'ubuntu' : [ 'xsltproc', 'docbook-utils', 'rake', 'docbook-xsl' ],
 	},
 	'run_post_install': (
 		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvmerge.exe',
-		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvtoolnix-gui.exe',
+		# '{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvtoolnix-gui.exe',
 		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvextract.exe',
 		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvinfo-gui.exe',
 		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvpropedit.exe',
-		'{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvinfo.exe',
+		# '{cross_prefix_bare}strip -v {product_prefix}/mkvtoolnix_git.installed/bin/mkvinfo.exe',
 	),
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'mkvtoolnix' },
 
