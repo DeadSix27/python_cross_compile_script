dee594c4fce3da569bce9444677abf2118b8ba4c
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-04 21:48:37 +0200

    packages/boost: re-enable
    forgot to re-enable this and it's needed for mkvtoolnix.

--- packages/dependencies/boost.py
+++ packages/dependencies/boost.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://sourceforge.net/projects/boost/files/boost/1.68.0/boost_1_68_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7f6130bc3cf65f56a618888ce9d5ea704fa10b462be126ad053e80e553d6d8b7' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/boost_1_68_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7f6130bc3cf65f56a618888ce9d5ea704fa10b462be126ad053e80e553d6d8b7' }, ], },
+		{ 'url' : 'https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/boost_1.71.0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee' }, ], },
 	],
 	'needs_make' :False,
 	'needs_make_install' :False,
@@ -16,5 +16,5 @@
 		'if [ ! -f "already_ran_make_0" ] ; then touch already_ran_make_0 ; fi',
 	],
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/boost/files/boost/', 'type' : 'sourceforge', 'regex' : r'(?P<version_num>[\d.]+)\.beta\.(?P<rc_num>[0-9])', },
-	'_info' : { 'version' : '1.68.0', 'fancy_name' : 'Boost' },
+	'_info' : { 'version' : '1.71.0', 'fancy_name' : 'Boost' },
 }
