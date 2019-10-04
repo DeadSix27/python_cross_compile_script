0b1a7de46cd8a1fd0db2f6fcbfc9c6f3e5e50c06
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-04 10:02:44 +0200

    packages/svt_vp9: remove obsolete patch

--- packages/dependencies/svt_vp9.py
+++ packages/dependencies/svt_vp9.py
@@ -4,9 +4,6 @@
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DCPPAN_BUILD=OFF -DCMAKE_BUILD_TYPE=Release',
-	'patches': [
-		('https://github.com/OpenVisualCloud/SVT-VP9/pull/59.patch', '-p1', '..'),  # "StaticLib: Add static library support #59" not yet merged, but seems finished.
-	],
 	'run_post_patch' : [
 		'sed -i.bak \'s/#include <Windows.h>/#include <windows.h>/\' ../Source/Lib/Codec/EbThreads.h',
 	],
