27e5b8b16f2beb3f336a8864cce2d5f4bca53c49
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-06 15:33:26 +0200

    packages/flac: disable fortify

--- packages/products/flac.py
+++ packages/products/flac.py
@@ -2,6 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://git.xiph.org/flac.git',
 	'configure_options' : '--host={target_host} --prefix={product_prefix}/flac_git.installed --disable-shared --enable-static',
+	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'depends_on' : [
 		'libogg',
 	],
