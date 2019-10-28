5e75f234f4acb35b4565276961bf83b424727ed3
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-28 13:11:23 +0100

    packages/sndfile: switch to cmake

--- packages/dependencies/libsndfile.py
+++ packages/dependencies/libsndfile.py
@@ -1,17 +1,9 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/erikd/libsndfile.git',
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-sqlite --disable-test-coverage --enable-external-libs --enable-experimental',
-	'run_post_patch' : [
-		'autoreconf -fiv -I M4',
-	],
-	'run_post_install' : [
-		'sed -i.bak \'s/Libs: -L${{libdir}} -lsndfile/Libs: -L${{libdir}} -lsndfile -lFLAC -lvorbis -lvorbisenc -logg -lspeex/\' "{pkg_config_path}/sndfile.pc"', #issue with rubberband not using pkg-config option "--static" or so idk?
-	],
-	'cflag_addition': '-D_FORTIFY_SOURCE=0',
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_PROGRAMS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DENABLE_BOW_DOCS=OFF -DENABLE_STATIC_RUNTIME=ON -DCMAKE_BUILD_TYPE=Release',
 	'depends_on' : [ 'libspeex' ],
-	'packages' : {
-		'arch' : [ 'autogen' ],
-	},
 	'_info' : { 'version' : None, 'fancy_name' : 'libsndfile' },
 }
