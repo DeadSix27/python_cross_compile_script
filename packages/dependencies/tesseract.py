3b9471494dda1ab8e88b7d0d8f67ddcee12c5c6f
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-30 20:19:37 +0100

    packages/tesseract: disable SW and remove obsolete config option

--- packages/dependencies/tesseract.py
+++ packages/dependencies/tesseract.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/tesseract-ocr/tesseract.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DBUILD_TRAINING_TOOLS=0 -DBUILD_TESTS=0 -DSTATIC=1 -DHAVE_LIBARCHIVE=0 -DLIBRARY_TYPE=STATIC -DCPPAN_BUILD=OFF -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DBUILD_TRAINING_TOOLS=0 -DSW_BUILD=0 -DBUILD_TESTS=0 -DSTATIC=1 -DHAVE_LIBARCHIVE=0 -DLIBRARY_TYPE=STATIC -DCMAKE_BUILD_TYPE=Release',
 	'depends_on' : [ 'leptonica', 'libxml2' ],
 	'run_post_patch' : [
 		'sed -i.bak \'s/set(LIB_Ws2_32 Ws2_32)/set(LIB_Ws2_32 ws2_32)/\' ../CMakeLists.txt',
