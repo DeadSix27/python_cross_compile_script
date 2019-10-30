73eed7c5c62eb48a4a93a6695b89f39c0c01888d
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-30 14:34:12 +0100

    packages/opencl: fix building

--- packages/dependencies/opencl_icd.py
+++ packages/dependencies/opencl_icd.py
@@ -1,18 +1,18 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/KhronosGroup/OpenCL-ICD-Loader.git',
-	'branch' : '4adae11f4e87a3a5b2afbb4b8aa1f3cd4f167833',
 	'needs_make_install' : False,
+	'source_subfolder': '_build',
 	'conf_system' : 'cmake',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=ON',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=ON -DOPENCL_ICD_LOADER_REQUIRE_WDK=OFF',
 	'depends_on' : [ 'opencl_headers' ],	
 	'run_post_build' : [
 		'if [ ! -f "already_ran_make_install" ] ; then cp -vf "libOpenCL.dll.a" "{target_prefix}/lib/libOpenCL.dll.a" ; fi',
 		'if [ ! -f "already_ran_make_install" ] ; then touch already_ran_make_install ; fi',
 	],
 	'patches' : [
-		('opencl/0001-OpenCL-git-prefix.patch','-p1'),
-		('opencl/0002-OpenCL-git-header.patch','-p1'),
+		('opencl/0001-OpenCL-git-prefix.patch', '-p1', '..'),
+		('opencl/0002-OpenCL-git-header.patch', '-p1', '..'),
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'OpenCL ICD' },
 }
