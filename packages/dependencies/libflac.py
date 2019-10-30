5b85129a2e41a5952ee1af7c7ac417274e6d3f8e
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-30 13:43:53 +0100

    packages/flac: switch to cmake and patch for mingw

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -1,8 +1,13 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://git.xiph.org/flac.git',
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
+	'patches': [
+		('flac/0001-mingw-fix.patch', '-p1', '..'),
+	],
 	'depends_on' : [
 		'libogg',
 	],
