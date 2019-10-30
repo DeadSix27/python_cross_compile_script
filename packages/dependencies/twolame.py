35bdfa7f8a30dc6ef14faf1f04f73f20e4e6c3ff
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-30 13:53:31 +0100

    packages/twolame: update to 0.4.0

--- packages/dependencies/twolame.py
+++ packages/dependencies/twolame.py
@@ -1,10 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://github.com/njh/twolame/releases/download/0.3.13/twolame-0.3.13.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '98f332f48951f47f23f70fd0379463aff7d7fb26f07e1e24e42ddef22cc6112a' }, ], },
-		{ 'url' : 'https://sourceforge.net/projects/twolame/files/twolame/0.3.13/twolame-0.3.13.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '98f332f48951f47f23f70fd0379463aff7d7fb26f07e1e24e42ddef22cc6112a' }, ], },
+		{ 'url' : 'https://github.com/njh/twolame/releases/download/0.4.0/twolame-0.4.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'cc35424f6019a88c6f52570b63e1baf50f62963a3eac52a03a800bb070d7c87d' }, ], },
+		{ 'url' : 'https://sourceforge.net/projects/twolame/files/twolame/0.4.0/twolame-0.4.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'cc35424f6019a88c6f52570b63e1baf50f62963a3eac52a03a800bb070d7c87d' }, ], },
 	],
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static CPPFLAGS=-DLIBTWOLAME_STATIC',
+	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static', #  CPPFLAGS=-DLIBTWOLAME_STATIC
 	'update_check' : { 'url' : 'https://github.com/njh/twolame/releases/', 'type' : 'githubreleases', 'name_or_tag' : 'tag_name' },
-	'_info' : { 'version' : '0.3.13', 'fancy_name' : 'twolame' },
+	'_info' : { 'version' : '0.4.0', 'fancy_name' : 'twolame' },
 }
