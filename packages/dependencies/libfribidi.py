12118bc0b40b2b18417a0f1fe93707fef4295eb6
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-10-30 13:54:42 +0100

    packages/fribidi: switch to meson

--- packages/dependencies/libfribidi.py
+++ packages/dependencies/libfribidi.py
@@ -1,15 +1,18 @@
 {
-	#https://raw.githubusercontent.com/rdp/ffmpeg-windows-build-helpers/master/patches/fribidi.diff
 	'repo_type' : 'git',
-	'do_not_bootstrap' : True,
-	'patches' : [
-		( 'fribidi/0001-mingwcompat-remove-doc-tests.patch', '-p1' ),
-	],
-	'run_post_patch' : [
-		'autoreconf -fiv',
-	],
-	#'branch' : 'c8fb314d9cab3e4803054eb9829373f014684dc0', # 'b534ab2642f694c3106d5bc8d0a8beae60bf60d3',
 	'url' : 'https://github.com/fribidi/fribidi.git',
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-docs',
+	'conf_system' : 'meson',
+	'build_system' : 'ninja',
+	'source_subfolder' : 'build',
+	'configure_options' :
+		'--prefix={target_prefix} '
+		'--libdir={target_prefix}/lib '
+		'--default-library=static '
+		'--buildtype=plain '
+		'--backend=ninja '
+		'-Ddocs=false '
+		'--buildtype=release '
+		'--cross-file={meson_env_file} ./ ..'
+	,
 	'_info' : { 'version' : None, 'fancy_name' : 'libfribidi' },
 }
