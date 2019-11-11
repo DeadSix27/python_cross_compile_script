af40aca5f9bd49ee0bd20c67000352eea3276de1
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-11 17:34:17 +0100

    packages/tiff: update to 4.1.0

--- packages/dependencies/libtiff.py
+++ packages/dependencies/libtiff.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://download.osgeo.org/libtiff/tiff-4.0.10.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/tiff-4.0.10.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4' }, ], },
+		{ 'url' : 'https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/tiff-4.1.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634' }, ], },
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
 	'depends_on' : [
@@ -12,5 +12,5 @@
 		'sed -i.bak \'s/Libs: -L${{libdir}} -ltiff$/Libs: -L${{libdir}} -ltiff -lwebp -llzma -ljpeg -lz/\' "{pkg_config_path}/libtiff-4.pc"',
 	],
 	'update_check' : { 'url' : 'https://download.osgeo.org/libtiff/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'tiff-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '4.0.10', 'fancy_name' : 'libtiff' },
+	'_info' : { 'version' : '4.1.0', 'fancy_name' : 'libtiff' },
 }
