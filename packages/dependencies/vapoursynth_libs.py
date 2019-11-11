c841354b06b1d2a8e10c8095c510a93cf8db457b
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-11 18:16:42 +0100

    packages/vapoursynth: update to R48

--- packages/dependencies/vapoursynth_libs.py
+++ packages/dependencies/vapoursynth_libs.py
@@ -4,9 +4,9 @@
 	'needs_configure' : False,
 	'needs_make_install' : False,
 	'depends_on' : [ 'python3_libs' ],
-	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R47',
+	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R48',
 	'packages' : {
 		'arch' : [ '7za' ],
 	},
-	'_info' : { 'version' : 'R47', 'fancy_name' : 'VapourSynth (library-only)' },
+	'_info' : { 'version' : 'R48', 'fancy_name' : 'VapourSynth (library-only)' },
 }
