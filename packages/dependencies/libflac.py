790f6cb406f2bc41caa812f5193ba65818b51677
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-12 14:49:41 +0100

    packages/flac: use patch instead of variables

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -3,10 +3,11 @@
 	'url' : 'https://github.com/xiph/flac',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DCMAKE_INSTALL_DATAROOTDIR={pkg_config_path} -DCMAKE_INSTALL_DATADIR={pkg_config_path} -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'patches': [
 		('flac/0001-mingw-fix.patch', '-p1', '..'),
+		('https://github.com/DeadSix27/flac/commit/d8f189ac0e5d0f051e5d844bc21c253ce505e18e.patch', '-p1', '..'),
 	],
 	'depends_on' : [
 		'libogg',
