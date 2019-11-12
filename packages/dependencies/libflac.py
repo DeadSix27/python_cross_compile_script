c1bff1448558243de76076207bfc00a9bc5ef0df
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-12 15:00:56 +0100

    packegs/flac: provide a version for the pkgconfig file
    We builg from git, hence this variable is not set, we'll provide the current stable version so that it gets into the pkg-config file and programs using it will be satisfied.

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/xiph/flac',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'patches': [
 		('flac/0001-mingw-fix.patch', '-p1', '..'),
