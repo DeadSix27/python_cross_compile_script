c6a227960514da791cf365bb9158a42c7a2195cf
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-12 14:21:08 +0100

    packages/flac: fix pkg-config install
    Why the.. would they use 2 different paths and variables for the pkg-config files? `DATAROOTDIR`  and `DATADIR`, instead of the proper `CMAKE_INSTALL_LIBDIR` ???
    
    I guess used those variables for the .cmake files but forgot to change it for the .pc files..

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -1,9 +1,9 @@
 {
 	'repo_type' : 'git',
-	'url' : 'https://git.xiph.org/flac.git',
+	'url' : 'https://github.com/xiph/flac',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DENABLE_STATIC_RUNTIME=ON -DCMAKE_INSTALL_DATAROOTDIR={pkg_config_path} -DCMAKE_INSTALL_DATADIR={pkg_config_path} -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DCMAKE_BUILD_TYPE=Release',
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'patches': [
 		('flac/0001-mingw-fix.patch', '-p1', '..'),
