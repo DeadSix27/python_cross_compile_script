b1343fb188e074fe44f8b52bce3a16a9d48a90c3
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-11-12 15:39:58 +0100

    packages/libopus: add patch to fix the incorrect pkg-config version

--- packages/dependencies/libopus.py
+++ packages/dependencies/libopus.py
@@ -4,10 +4,11 @@
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={product_prefix}/opus_git.installed -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DOPUS_CUSTOM_MODES=1 -DOPUS_BUILD_PROGRAMS=0 -DOPUS_INSTALL_PKG_CONFIG_MODULE=1',
-	# 'patches' : [
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DOPUS_STACK_PROTECTOR=0 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=0 -DBUILD_TESTING=0 -DOPUS_CUSTOM_MODES=1 -DOPUS_BUILD_PROGRAMS=0 -DOPUS_INSTALL_PKG_CONFIG_MODULE=1',
+	'patches' : [
+		('https://github.com/DeadSix27/opus/commit/3df389619cf39e73d8338819d27958c67d67663a.patch', '-p1', '..'),
 		# ('opus/opus_git_strip_declspec.patch', '-p1'),
-	# ],
+	],
 	'run_post_install' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lopus.*/Libs: -L${{libdir}} -lopus -lssp/\' "{pkg_config_path}/opus.pc"', # ???, keep checking whether this is needed, apparently it is for now.
 	],
