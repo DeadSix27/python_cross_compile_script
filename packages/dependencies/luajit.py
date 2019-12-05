6a2a4e2cb881ddea3f2eb04b8155f513987b3203
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-05 23:00:25 +0100

    packages: add various missing dependencies

--- packages/dependencies/luajit.py
+++ packages/dependencies/luajit.py
@@ -5,5 +5,6 @@
 	'custom_cflag' : '-O3', # doesn't like march's past ivybridge (yet), so we override it.
 	'install_options' : 'CROSS={cross_prefix_bare} HOST_CC="gcc -m{bit_num}" TARGET_SYS=Windows BUILDMODE=static FILE_T=luajit.exe PREFIX={target_prefix}',
 	'build_options' : 'CROSS={cross_prefix_bare} HOST_CC="gcc -m{bit_num}" TARGET_SYS=Windows BUILDMODE=static amalg',
+	'depends_on': [ 'libdl' ],
 	'_info' : { 'version' : None, 'fancy_name' : 'LuaJIT2' },
 }
--- packages/dependencies/opusfile.py
+++ packages/dependencies/opusfile.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/xiph/opusfile.git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
 	'depends_on' : [
-		'libressl', 'libopus'
+		'libressl', 'libopus', 'libogg'
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'opusfile' },
 }
