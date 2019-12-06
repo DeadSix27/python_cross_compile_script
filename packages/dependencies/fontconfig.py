3ee806c59e1bcf00393ee1a296cd27db2a394bd6
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-06 16:00:18 +0100

    packages/fontconfig: build with json-c

--- packages/dependencies/fontconfig.py
+++ packages/dependencies/fontconfig.py
@@ -1,14 +1,13 @@
 {
 	'repo_type' : 'git',
 	'do_not_bootstrap' : True,
-	# 'branch' : '40e27f5d989ef4e40d218624de0a51de3de43177',
 	'url' : 'https://gitlab.freedesktop.org/fontconfig/fontconfig.git',
 	'folder_name' : 'fontconfig_git',
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-libxml2 --disable-docs --disable-silent-rules',
+	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-libxml2 --disable-docs --disable-silent-rules --with-expat',
 	'patches' : [
 		('fontconfig/0001-fontconfig-remove-tests.patch', '-p1' ),
-		('fontconfig/fontconfig-git-utimes.patch', '-p1' ),
-		# ['https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/patches/fontconfig/fontconfig-0001-fix-missing-bracket.patch', '-p1' ],
+		# ('fontconfig/fontconfig-git-utimes.patch', '-p1' ),
+		# ('fontconfig/fontconfig-0001-fix-missing-bracket.patch', '-p1' ),
 	],
 	'run_post_patch' : [
 		'autoreconf -fiv',
@@ -17,11 +16,8 @@
 		# 'sed -i.bak \'s/-L${{libdir}} -lfontconfig[^l]*$/-L${{libdir}} -lfontconfig -lfreetype -lharfbuzz -lxml2 -liconv -lintl/\' "{pkg_config_path}/fontconfig.pc"',
 	# ],
 	'depends_on' : [
-		'iconv','libxml2','freetype',
+		'expat', 'iconv', 'libxml2', 'freetype', 'json-c',
 	],
-	'packages' : {
-		'arch' : [ 'gperf' ],
-	},
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : None, 'fancy_name' : 'fontconfig' },
 }
