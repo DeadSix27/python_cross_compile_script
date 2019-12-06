710304cff1abd965f331e5b25c4900d2d757a38e
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-06 18:23:01 +0100

    packages/twolame: does not like sysroot being set..

--- packages/dependencies/twolame.py
+++ packages/dependencies/twolame.py
@@ -4,7 +4,7 @@
 		{ 'url' : 'https://github.com/njh/twolame/releases/download/0.4.0/twolame-0.4.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'cc35424f6019a88c6f52570b63e1baf50f62963a3eac52a03a800bb070d7c87d' }, ], },
 		{ 'url' : 'https://sourceforge.net/projects/twolame/files/twolame/0.4.0/twolame-0.4.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'cc35424f6019a88c6f52570b63e1baf50f62963a3eac52a03a800bb070d7c87d' }, ], },
 	],
-	'configure_options' : '{autoconf_prefix_options}', #  CPPFLAGS=-DLIBTWOLAME_STATIC
+	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static', #  CPPFLAGS=-DLIBTWOLAME_STATIC
 	'depends_on' : ['libsndfile', ],
 	'update_check' : { 'url' : 'https://github.com/njh/twolame/releases/', 'type' : 'githubreleases', 'name_or_tag' : 'tag_name' },
 	'_info' : { 'version' : '0.4.0', 'fancy_name' : 'twolame' },
