41f5d92b1db0c80e4efa5799e963f84144cd69ff
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-06 22:05:03 +0100

    packages/ffmpeg-*: add sdl for the product variants
    Forgot it's required for ffplay

--- packages/products/ffmpeg_shared.py
+++ packages/products/ffmpeg_shared.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'git://git.ffmpeg.org/ffmpeg.git',
 	'rename_folder' : 'ffmpeg_shared_git',
-	'configure_options' : '!VAR(ffmpeg_min_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_shared_git.installed --enable-shared --disable-static --disable-libbluray --disable-libgme',
-	'depends_on' : ['ffmpeg_depends_min', 'ffmpeg_depends_extra', 'ffmpeg_depends_nonfree'],
+	'configure_options' : '!VAR(ffmpeg_min_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_shared_git.installed --enable-sdl --enable-shared --disable-static --disable-libbluray --disable-libgme',
+	'depends_on' : ['ffmpeg_depends_min', 'ffmpeg_depends_extra', 'ffmpeg_depends_nonfree', 'sdl2'],
 	'_info' : { 'version' : None, 'fancy_name' : 'ffmpeg (shared)' },
 }
--- packages/products/ffmpeg_shared_min.py
+++ packages/products/ffmpeg_shared_min.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'git://git.ffmpeg.org/ffmpeg.git',
 	'rename_folder' : 'ffmpeg_shared_git',
-	'configure_options' : '!VAR(ffmpeg_min_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_shared_min_git.installed --enable-shared --disable-static --disable-libbluray --disable-libgme',
-	'depends_on' : [ 'ffmpeg_depends_min', 'ffmpeg_depends_nonfree' ],
+	'configure_options' : '!VAR(ffmpeg_min_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_shared_min_git.installed --enable-sdl --enable-shared --disable-static --disable-libbluray --disable-libgme',
+	'depends_on' : [ 'ffmpeg_depends_min', 'ffmpeg_depends_nonfree', 'sdl2'],
 	'_info' : { 'version' : None, 'fancy_name' : 'ffmpeg (shared, min)' },
 }
