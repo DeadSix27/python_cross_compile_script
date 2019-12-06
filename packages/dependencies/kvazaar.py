48832dff75f05f0df958e8e8e5c3ab7cf9b72cf9
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-06 17:10:21 +0100

    packages/kvazaar: use patch instead of sed

--- packages/dependencies/kvazaar.py
+++ packages/dependencies/kvazaar.py
@@ -2,8 +2,8 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/ultravideo/kvazaar.git',
 	'configure_options' : '{autoconf_prefix_options}',
-	'run_post_patch' : [
-		'sed -i.bak "s/KVZ_PUBLIC const kvz_api/const kvz_api/g" src/kvazaar.h',
+	'patches' : [
+		( 'kvazaar/0001-mingw-workaround.patch', '-p1' ),
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'kvazaar' },
 }
