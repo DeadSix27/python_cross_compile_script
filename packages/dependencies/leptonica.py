0e382edf6580bcfcb79b966dcaf952b05b9ee497
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-07 18:55:07 +0100

    packages/leptonica: cleanup

--- packages/dependencies/leptonica.py
+++ packages/dependencies/leptonica.py
@@ -3,10 +3,8 @@
 	'url' : 'https://github.com/DanBloomberg/leptonica.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'env_exports' : {
-		'CPPFLAGS' : '-DOPJ_STATIC',
-		'CFLAGS' : '-DOPJ_STATIC'
-	},
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DSW_BUILD=0 -DBUILD_PROG=0 -DBUILD_SHARED_LIBS=0 -DSTATIC=1 -DLIBRARY_TYPE=STATIC -DCMAKE_BUILD_TYPE=Release',
+	'cflag_addition': '-DOPJ_STATIC',
 	'run_post_patch' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -l@leptonica_NAME@/Libs: -L${{libdir}} -l@leptonica_NAME@-@leptonica_VERSION@/\' ../lept.pc.cmake',
 	],
@@ -15,7 +13,5 @@
 		'sed -i \'s/Libs: -L${{libdir}} -lleptonica-\(.*\)$/Libs: -L${{libdir}} -lleptonica-\\1 -lgif/\' "{pkg_config_path}/lept.pc"',
 	],
 	'depends_on' : [ 'zlib', 'libopenjpeg', 'libpng', 'libwebp', 'dlfcn-win32', 'libjpeg-turbo', 'giflib', 'libtiff', ],
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DSW_BUILD=0 -DBUILD_PROG=0 -DBUILD_SHARED_LIBS=0 -DSTATIC=1 -DLIBRARY_TYPE=STATIC -DCMAKE_BUILD_TYPE=Release',
 	'_info' : { 'version' : None, 'fancy_name' : 'tesseract' },
 }
-#TODO: Add GIF/TIFF Library?
