72f911a5d0df113d863faa6224f8baf4a2095a1f
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-07 16:54:20 +0100

    packages/zlib: switch to cmake

--- packages/dependencies/zlib.py
+++ packages/dependencies/zlib.py
@@ -1,16 +1,11 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/madler/zlib.git',
-	'env_exports' : {
-		'AR' : '{cross_prefix_bare}ar',
-		'CC' : '{cross_prefix_bare}gcc',
-		'PREFIX' : '{target_prefix}',
-		'RANLIB' : '{cross_prefix_bare}ranlib',
-		'LD'     : '{cross_prefix_bare}ld',
-		'STRIP'  : '{cross_prefix_bare}strip',
-		'CXX'    : '{cross_prefix_bare}g++',
-	},
-	'configure_options' : '--static --prefix={target_prefix}',
-	'build_options' : '{make_prefix_options} ARFLAGS=rcs',
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DAMD64=ON -DCMAKE_BUILD_TYPE=Release',
+	'patches' : [
+		('zlib/0001-remove-tests-examples.patch', '-p1', '..'),
+	],
 	'_info' : { 'version' : None, 'fancy_name' : 'zlib' },
 }
