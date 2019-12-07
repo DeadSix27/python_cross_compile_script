9a9d968f979dd12ee2bf4b1922aa1e0d70d27974
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-07 18:55:21 +0100

    packages/ogg: switch to cmake

--- packages/dependencies/libogg.py
+++ packages/dependencies/libogg.py
@@ -1,7 +1,8 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/xiph/ogg.git',
-	# 'folder_name' : 'ogg-1.3.2',
-	'configure_options' : '{autoconf_prefix_options}',
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
 	'_info' : { 'version' : None, 'fancy_name' : 'ogg' },
 }
