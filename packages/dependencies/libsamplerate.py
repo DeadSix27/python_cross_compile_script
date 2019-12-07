8931e85404c1f938d055a2f4d14186a2f1f0bb16
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-07 19:00:32 +0100

    packages/samplerate: switch to cmake

--- packages/dependencies/libsamplerate.py
+++ packages/dependencies/libsamplerate.py
@@ -1,11 +1,9 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/erikd/libsamplerate.git',
-	'configure_options' : '{autoconf_prefix_options} --disable-alsa',
-	'depends_on' : [
-		'libflac',
-		'fftw',
-		'libopus',
-	],
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DLIBSAMPLERATE_EXAMPLES=OFF -DLIBSAMPLERATE_TESTS=OFF -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
+	'depends_on' : ['libflac', 'fftw', 'libopus',],
 	'_info' : { 'version' : None, 'fancy_name' : 'libsamplerate' },
 }
