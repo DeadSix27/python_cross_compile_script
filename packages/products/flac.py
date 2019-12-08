24fd65b4f77fbe48c2b3edfb5a8dc1ecb594996c
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-08 09:00:26 +0100

    packages/flac: use github repo

--- packages/products/flac.py
+++ packages/products/flac.py
@@ -1,6 +1,6 @@
 {
 	'repo_type' : 'git',
-	'url' : 'https://git.xiph.org/flac.git',
+	'url' : 'https://github.com/xiph/flac.git',
 	'configure_options' : '--host={target_host} --prefix={output_prefix}/flac_git.installed --disable-shared --enable-static',
 	'custom_cflag' : '-O3 -D_FORTIFY_SOURCE=0',
 	'depends_on' : [
