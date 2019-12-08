62de23290365d2c8deb8f8ceaaebc851d6fe7150
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-08 21:18:10 +0100

    packages/zlib: disable asm
    crashes ffmpeg png support (was worh a try)

--- packages/dependencies/zlib.py
+++ packages/dependencies/zlib.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/madler/zlib.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" -DBUILD_SHARED_LIBS=0 -DAMD64=ON -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
 	'patches' : [
 		('zlib/0001-mingw-workarounds.patch', '-p1', '..'),
 	],
