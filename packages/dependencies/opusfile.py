41db1565e700ef853b3b4f0f9d79545b492c7853
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-08 09:29:14 +0100

    package/opusfile: disable shallow clone (for version.sh)

--- packages/dependencies/opusfile.py
+++ packages/dependencies/opusfile.py
@@ -1,6 +1,7 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/xiph/opusfile.git',
+	'depth_git': 0,
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
 	'depends_on' : [
 		'libressl', 'libopus', 'libogg'
