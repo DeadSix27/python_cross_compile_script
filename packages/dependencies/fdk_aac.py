e7021061fdd2a2ea1638a6a302a0500f6e46b6fe
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-22 19:08:04 +0100

    packages/fdk_aac: add note about optimisations

--- packages/dependencies/fdk_aac.py
+++ packages/dependencies/fdk_aac.py
@@ -4,6 +4,9 @@
 		'autoreconf -fiv',
 	],
 	'url' : 'https://github.com/mstorsjo/fdk-aac.git',
+	# Note does not like to be compiled without optimisations: error: inlining failed in call to always_inline 'void fft_4(FIXP_DBL*)': indirect function call with a yet undetermined callee
+	# So make sure to empty the cflags or enable optimisations when your global c(xx)flags are defaulting to -0g/0:
+	# 'custom_cflag': '',
 	'configure_options' : '{autoconf_prefix_options}',
 	'_info' : { 'version' : None, 'fancy_name' : 'fdk-aac' },
 }
