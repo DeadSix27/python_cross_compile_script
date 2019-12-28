07550430d8e302f57ede4e584fe1c39f8b1c0f0c
DeadSix27 <DeadSix27@users.noreply.github.com>
2019-12-28 23:38:45 +0100

    packages/mfx_quicksync: remove patch
    Fix got merged upstream https://github.com/lu-zero/mfx_dispatch/pull/70

--- packages/dependencies/intel_quicksync_mfx.py
+++ packages/dependencies/intel_quicksync_mfx.py
@@ -4,9 +4,6 @@
 	'run_post_patch' : [
 		'autoreconf -fiv',
 	],
-	'patches' :	[
-		( 'https://github.com/lu-zero/mfx_dispatch/pull/70.patch', '-p1' ),
-	],
 	'url' : 'https://github.com/lu-zero/mfx_dispatch.git',
 	'configure_options' : '{autoconf_prefix_options} --without-libva_drm --without-libva_x11',
 	'_info' : { 'version' : None, 'fancy_name' : 'intel_quicksync_mfx' },
