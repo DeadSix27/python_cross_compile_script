80674c1f9c1a906b21d0ba164a69b4f44f6114fa
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-01-09 15:36:08 +0100

    packages/mujs: hardcode stable version
    because master builds use hashes and mpv checks for actual version, could probably edit the git describe command in the Makefile, but this will do.

--- packages/dependencies/mujs.py
+++ packages/dependencies/mujs.py
@@ -1,12 +1,24 @@
 {
 	'repo_type' : 'git',
 	'url' : 'git://git.ghostscript.com/mujs.git',
-	# 'branch' : '3430d9a06d6f8a3696e2bbdca7681937e60ca7a9',
 	'needs_configure' : False,
 	'build_options' : '{make_prefix_options} prefix={target_prefix} HAVE_READLINE=no',
 	'install_options' : '{make_prefix_options} prefix={target_prefix} HAVE_READLINE=no',
-	'run_post_patch' : [
-		'sed -i.bak \'s/install -m 755 $(OUT)\/mujs $(DESTDIR)$(bindir)/install -m 755 $(OUT)\/mujs.exe $(DESTDIR)$(bindir)/g\' Makefile',
-	],
+	'regex_replace': {
+		'post_patch': [
+			{
+				0: r'install -m 755 \$\(OUT\)\/mujs',
+				1: r'install -m 755 $(OUT)/mujs.exe',
+				'in_file': 'Makefile'
+			},
+		],
+		'post_install': [ # hardcode version because master builds use hashes and mpv checks for actual version, could probably edit the git describe command in the Makefile, but this will do.
+			{
+				0: r'^Version:([\n\r\s]+)?[^\n]+$',
+				1: r'Version: 1.0.6',
+				'in_file': '{pkg_config_path}/mujs.pc'
+			},
+		],
+	},
 	'_info' : { 'version' : None, 'fancy_name' : 'mujs' },
 }
