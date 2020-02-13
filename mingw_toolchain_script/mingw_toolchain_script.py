69dec6f455e03a191be6e896eb6793fc3222142d
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-02-13 20:34:52 +0100

    mingw_script: update gmp and gcc

--- mingw_toolchain_script/mingw_toolchain_script.py
+++ mingw_toolchain_script/mingw_toolchain_script.py
@@ -58,7 +58,7 @@ SOURCES['mingw-w64'] = {
 }
 SOURCES['gmp'] = {
 	'type': 'archive',
-	'version': '6.1.2',
+	'version': '6.2.0',
 	'url': 'https://ftp.gnu.org/gnu/gmp/gmp-{version}.tar.xz',
 	'update_check': {'url': 'https://ftp.gnu.org/gnu/gmp/', 'type': 'httpindex', 'regex': r'gmp-(?P<version_num>[\d.]+)\.tar\.xz'},
 }
@@ -95,10 +95,10 @@ SOURCES['binutils'] = {
 }
 SOURCES['gcc'] = {
 	'type': 'archive',
-	# 'version'   : '9.2.0',
+	# 'version'   : '9.2.0', # stable versions
 	# 'url' : 'https://gcc.gnu.org/pub/gcc/releases/gcc-{version}/gcc-{version}.tar.xz',
-	'version': '9-20191130',
-	'url': 'https://gcc.gnu.org/pub/gcc/snapshots/{version}/gcc-{version}.tar.xz',
+	'version': '9-20200208', # pre-release versions
+	'url': 'ftp://ftp.fu-berlin.de/unix/languages/gcc/snapshots/{version}/gcc-{version}.tar.xz',
 	'patches': [
 		#( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/mingw_toolchain_script/patches/0001-gcc_7_1_0_weak_refs_x86_64.patch', 'p1' ),
 		# ( 'https://raw.githubusercontent.com/DeadSix27/python_cross_compile_script/master/mingw_toolchain_script/patches/0140-gcc-7-Enable-std-experimental-filesystem.patch', 'p1' ), #Unable to get this to work.
