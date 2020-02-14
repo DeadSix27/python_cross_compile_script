f58b200ab08b7c08c759a68b86896eb12720c014
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-02-14 14:27:59 +0100

    packages/gmp: update to 6.2.0

--- packages/dependencies/gmp.py
+++ packages/dependencies/gmp.py
@@ -4,10 +4,10 @@
 	#generic_configure "ABI=$bits_target"
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/gmp-6.1.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912' }, ], },
+		{ 'url' : 'https://gmplib.org/download/gmp/gmp-6.2.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '258e6cd51b3fbdfc185c716d55f82c08aff57df0c6fbd143cf6ed561267a1526' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/gmp-6.2.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '258e6cd51b3fbdfc185c716d55f82c08aff57df0c6fbd143cf6ed561267a1526' }, ], },
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
 	'update_check' : { 'url' : 'https://gmplib.org/download/gmp/', 'type' : 'httpindex', 'regex' : r'gmp-(?P<version_num>[\d.]+)\.tar\.xz' },
-	'_info' : { 'version' : '6.1.2', 'fancy_name' : 'gmp' },
+	'_info' : { 'version' : '6.2.0', 'fancy_name' : 'gmp' },
 }
