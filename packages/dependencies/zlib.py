85d8b9989185831305a93716bd79c7c3488c5fcb
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-03-10 08:35:58 +0100

    packages/pkg-config: build first

--- packages/dependencies/zlib.py
+++ packages/dependencies/zlib.py
@@ -4,6 +4,7 @@
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
+	'depends_on' : ['pkg-config', ],
 	'patches' : [
 		('zlib/0001-mingw-workarounds.patch', '-p1', '..'),
 	],
