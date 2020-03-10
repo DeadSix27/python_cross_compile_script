a474df4d684bc6e38c26fefcab5cab7e774f0f71
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-03-10 08:37:27 +0100

    packages/json-c: disable werror

--- packages/dependencies/json-c.py
+++ packages/dependencies/json-c.py
@@ -2,6 +2,6 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/json-c/json-c.git',
 	'conf_system' : 'cmake',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_RDRAND=ON -DENABLE_THREADING=ON',
+	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_RDRAND=ON -DENABLE_THREADING=ON -DDISABLE_WERROR=ON',
 	'_info' : { 'version' : None, 'fancy_name' : 'json-c' },
 }
