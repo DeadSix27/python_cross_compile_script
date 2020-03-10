e857132752d7b84395a5e769b449404a87c456d0
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-03-10 08:36:46 +0100

    packages/fribidi: update to newer working commit

--- packages/dependencies/libfribidi.py
+++ packages/dependencies/libfribidi.py
@@ -1,9 +1,9 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/fribidi/fribidi.git',
+	'depth_git': 0,
 	'conf_system' : 'meson',
-	'branch': 'f503e576c8345dd32d7438569346d6ca9aa50044',
-	'depth_git': 2,
+	'branch': 'f9e8e71a6fbf4a4619481284c9f484d10e559995',
 	'build_system' : 'ninja',
 	'source_subfolder' : 'build',
 	'configure_options' :
