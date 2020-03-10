f4ba489422a149a351daccee73acb2d5b7503d46
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-03-10 12:42:58 +0100

    vulkan: add pkgconfig work arounds

--- packages/dependencies/vulkan_loader.py
+++ packages/dependencies/vulkan_loader.py
@@ -6,7 +6,7 @@
 		'.. {cmake_prefix_options} -DVULKAN_HEADERS_INSTALL_DIR={target_prefix} '
 		'-DCMAKE_BUILD_TYPE=Release '
 		'-DBUILD_TESTS=OFF '
-		'-DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_ASM_COMPILER={mingw_binpath}/{cross_prefix_bare}as '
+		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
 		'-DBUILD_TESTS=OFF ' #-DENABLE_STATIC_LOADER=ON 
 	,
 	# 'cpu_count': 1,
@@ -16,6 +16,21 @@
 		# ('vulkan/0001-fix-cross-compiling.patch', '-p1', '..'),
 		('vulkan/0001-mingw-workarounds.patch','-p1','..'),
 	],
+	'regex_replace': {
+		'post_install': [
+			{
+				0: r'(?:[^\r\n]+)?libdir=(?:[^\r\n]+)?',
+				'in_file': '{pkg_config_path}/vulkan.pc',
+				'out_file': '{pkg_config_path}/vulkan.pc'
+			},
+			{
+				0: r'exec_prefix=([^\r\n]+)',
+				1: r'prefix={{target_prefix}}\nexec_prefix=\1\nlibdir=${{exec_prefix}}/lib\n',
+				'in_file': '{pkg_config_path}/vulkan.pc',
+				'out_file': '{pkg_config_path}/vulkan.pc'
+			},
+		]
+	},
 	'depends_on' : [ 'vulkan_headers' ], # 'vulkan-d3dheaders',
 	'_info' : { 'version' : None, 'fancy_name' : 'Vulkan Loader' },
 }
