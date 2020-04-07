15784ff6340a215a3c3fcdba365318182b660612
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-04-07 10:33:12 +0200

    packages/flac: remove patch

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -6,7 +6,6 @@
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_PROGRAMS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
 	'patches': [
 		('flac/0001-mingw-fix.patch', '-p1', '..'),
-		('https://github.com/DeadSix27/flac/commit/c8cb62effec6d9254a6304aac6a19c981d70eb83.patch', '-p1', '..')
 	],
 	'regex_replace': {
 		'post_patch': [
