c15c1e83a7dbae72d0024c40b1171c2be7819ae3
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-04-07 06:51:32 +0200

    packages/flac: fix pkg-config install path

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -3,9 +3,10 @@
 	'url' : 'https://github.com/xiph/flac.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_PROGRAMS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
 	'patches': [
 		('flac/0001-mingw-fix.patch', '-p1', '..'),
+		('https://github.com/DeadSix27/flac/commit/c8cb62effec6d9254a6304aac6a19c981d70eb83.patch', '-p1', '..')
 	],
 	'regex_replace': {
 		'post_patch': [
