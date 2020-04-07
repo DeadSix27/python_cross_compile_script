219aec8958f58302e2b24ece1d7029b3d18e5ad2
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-04-07 06:27:31 +0200

    use newest vapoursynth and python version

--- packages/dependencies/vapoursynth_libs.py
+++ packages/dependencies/vapoursynth_libs.py
@@ -4,9 +4,9 @@
 	'needs_configure' : False,
 	'needs_make_install' : False,
 	'depends_on' : [ 'python3_libs' ],
-	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R48',
+	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R49',
 	'packages' : {
 		'arch' : [ '7za' ],
 	},
-	'_info' : { 'version' : 'R48', 'fancy_name' : 'VapourSynth (library-only)' },
+	'_info' : { 'version' : 'R49', 'fancy_name' : 'VapourSynth (library-only)' },
 }
