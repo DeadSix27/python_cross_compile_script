e264d3806c8f39ffce40f20b2539d726a051723f
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-04-08 04:54:32 +0200

    packages/dav1d: add hack

--- packages/dependencies/libdav1d.py
+++ packages/dependencies/libdav1d.py
@@ -5,6 +5,20 @@
 	'build_system' : 'ninja',
 	'rename_folder' : 'libdav1d_git',
 	'source_subfolder' : 'build',
+	'regex_replace': { #hacky but works, so who cares, for some reason libdav1d thinks we have POSIX_MEMALIGN.. maybe mingw or gcc bug, .. so we'll just force it to not define that we have it so it doesn't use it.
+		'post_patch': [
+			{
+				0: r'cdata.set\(\'HAVE_POSIX_MEMALIGN\', 1\)',
+				1: 'cdata.set(\'HAVE_ALIGNED_MALLOC\', 1)',
+				'in_file': '../meson.build'
+			},
+			{
+				0: r'cdata.set\(\'HAVE_ALIGNED_MALLOC\', 1\)',
+				1: 'cdata.set(\'HAVE_ALIGNED_MALLOC\', 1)',
+				'in_file': '../meson.build'
+			},
+		],
+	},
 	'configure_options' :
 		'--prefix={target_prefix} '
 		'--libdir={target_prefix}/lib '
