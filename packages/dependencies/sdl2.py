c770f639bfab69b4e8350f3157d5eab57fc6ffb0
DeadSix27 <DeadSix27@users.noreply.github.com>
2020-05-08 22:22:16 +0200

    packages/sdl2: use stable release

--- packages/dependencies/sdl2.py
+++ packages/dependencies/sdl2.py
@@ -1,8 +1,13 @@
 {
 	'repo_type' : 'mercurial',
-	'url' : 'https://hg.libsdl.org/SDL',
-	'folder_name' : 'sdl2_hg',
+	# 'url' : 'https://hg.libsdl.org/SDL',
+	# 'folder_name' : 'sdl2_hg',
 
+	'repo_type' : 'archive',
+	'download_locations' : [
+		{ 'url' : 'https://www.libsdl.org/release/SDL2-2.0.12.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/SDL2-2.0.12.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '349268f695c02efbc9b9148a70b85e58cefbbf704abd3e91be654db7f1e2c863' }, ], },
+	],
 	'conf_system' : 'cmake',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DSDL_SHARED=OFF',
 	'source_subfolder': '_build',
