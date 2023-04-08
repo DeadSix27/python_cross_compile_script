143c397e368dc1b4c9d594940215bf48ab4e876c
DeadSix27 <DeadSix27@users.noreply.github.com>
2023-04-08 18:13:53 +0200

    for testing

--- packages/dependencies/freetype_lib-with-harfbuzz.py
+++ packages/dependencies/freetype_lib-with-harfbuzz.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
 		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.13.0/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
+		{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
 	],
 	#'custom_cflag' : '-O3',
